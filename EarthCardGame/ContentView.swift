0af0c62c9a7120c6689a6e0ac4c9d97acc5996d9
elinfelikman <elin1523@gmail.com>
2026-05-27 19:34:04 +0300

    Initial Commit

---
+++ EarthCardGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EarthCardGame
+//
+//  Created by Elin Felikman on 27/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EarthCardGame/EarthCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthCardGameApp.swift
+//  EarthCardGame
+//
+//  Created by Elin Felikman on 27/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct EarthCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
